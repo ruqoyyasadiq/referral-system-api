54594bc7ba90413e566718aa894dae4d0744c6f2
Rukayat Sadiq <fruqoyya@gmail.com>
2018-08-29 22:40:31 +0100

    spin up basic server

---
+++ src/server.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const cors = require('cors')
+const dotenv = require('dotenv')
+const bodyParser = require('body-parser')
+const logger = require('morgan')
+
+dotenv.config()
+
+const environment = process.env.NODE_ENV
+
+const app = express()
+
+if (environment === 'development') {
+  app.use(logger('dev'))
+}
+
+app.use(cors())
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Fetch port value from env var or use default
+const port = process.env.PORT || 8080;
+
+app.listen(port, () => {
+  console.info(`App started on port ${port} on ${environment} environment`)
+})
