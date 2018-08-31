bb48119de42c1d004743547718feac4f6f09521c
Rukayat Sadiq <fruqoyya@gmail.com>
2018-08-31 17:50:11 +0100

    modify event creation on referral creation

---
+++ src/constants.js
@@ -0,0 +1,3 @@
+const EVENT_TYPE_REFERRAL_CREATED = 'Referral_Awarded'
+
+module.exports = { EVENT_TYPE_REFERRAL_CREATED }
--- src/controllers/referral.controller.js
+++ src/controllers/referral.controller.js
@@ -2,35 +2,54 @@ const Referral = require('../models').Referral
 const Contact = require('../models').Contact
 const Event = require('../models').Event
 const { resourceExist } = require('../utils')
+const { EVENT_TYPE_REFERRAL_CREATED } = require('../constants')
 
 const REFERRAL_POINTS = process.env.REFERRAL_POINTS || 100 // set default value in case environment variable is not set.
 
 class referralController {
+  /**
+   * @description creates new Referral, updates Contact table, create Event for the action.
+   * @param  {object} req - request from the client to the server
+   * @param  {object} res - response from the server to the client
+   */
   static create(req, res) {
-    const { email, name, referrerId, pointsEarned } = req.body
-    // console.log(referrerId, 'body')
+    const { email, name, referrerId } = req.body
     return Referral.findOne({
       where: { email }
     }).then(referral => {
-      // console.log(referral, 'referral in controller')
       if (!resourceExist(req, res, referral)) {
         return Referral
-          .create({ email, name, pointsEarned, referrerId })
+          .create({ email, name, points: REFERRAL_POINTS, referrerId })
           .then(createdReferral => {
-            // console.log(createdReferral.dataValues, 'createdReferral')
             Contact.findOne({
               where: { id: referrerId },
-              attributes: ['points']
+              attributes: {
+                exclude: ['createdAt', 'updatedAt']
+              }
             }).then(contact => {
-              console.log(contact.points, 'contact in findOne')
               if(contact) {
-                const accruedPoints = contact.points + REFERRAL_POINTS
-                console.log(accruedPoints, 'accruedPoints')
-              } else {
-                
+                const accruedPoints = contact.points + parseInt(REFERRAL_POINTS)
+                const modifiedContact = contact.dataValues
+                modifiedContact.points = accruedPoints
+                Contact.update(modifiedContact, {
+                  where: { id: referrerId },
+                  returning: true
+                }).then(() => {
+                  Event.create({
+                    description: '',
+                    type: EVENT_TYPE_REFERRAL_CREATED,
+                    points: REFERRAL_POINTS,
+                    contactId: referrerId,
+                    referralId: createdReferral.dataValues.id
+                  })
+                })
               }
             })
-          })
+            res.status(200).send(createdReferral)
+          }
+        ).catch(error => {
+          res.status(500).send(error.toString())
+        })
       }
     })
   }
